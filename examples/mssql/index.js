a46d523a128556cbb7a559b711c521a2a04ee6fd
zetxx <zetxx@belogradchik.biz>
2022-07-29 16:02:25 +0300

    fix: reform

--- examples/mssql/index.js
+++ examples/mssql/index.js
@@ -1,22 +1,5 @@
-const sql = require('mssql');
-const lib = require('../../lib/mssql');
-
-const init = (async() => {
-    const {
-        server,
-        user,
-        password,
-        pool,
-        database,
-        options
-    } = require('../mssql.config.js');
-    const cPool = await sql.connect({
-        server,
-        user,
-        password,
-        pool,
-        database,
-        options
-    });
-    return cPool.request();
-});
+(async() => {
+    await require('../../lib/mssql')(
+        require('../mssql.config.js')
+    );
+})();
