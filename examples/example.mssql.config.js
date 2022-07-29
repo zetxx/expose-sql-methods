d274fbb228d7970ee2a1ef62d1296f27a27ea676
zetxx <zetxx@belogradchik.biz>
2022-07-29 16:47:55 +0300

    fix: example config

--- examples/example.mssql.config.js
+++ examples/example.mssql.config.js
@@ -1,15 +1,22 @@
 module.exports = {
-    server: 'localhost',
-    user: 'sa',
-    password: '*******',
-    database: 'migration-test',
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
+    connect: {
+        server: 'localhost',
+        user: 'sa',
+        password: '***',
+        database: 'mfactor',
+        pool: {
+            max: 10,
+            min: 0,
+            idleTimeoutMillis: 60000
+        },
+        options: {
+            encrypt: true, // for azure
+            trustServerCertificate: true // change to true for local dev / self-signed certs
+        }
     },
-    options: {
-        encrypt: true, // for azure
-        trustServerCertificate: true // change to true for local dev / self-signed certs
+    link: {
+        SP: {
+            schemas: ['a', 'b'] // schemas allowed
+        }
     }
 };
