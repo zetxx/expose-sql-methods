37653b35251dae44e9d04eb10636626a23b1d423
Elin Angelow <elin.angelov@shelly.com>
2023-12-19 16:46:11 +0200

    fix: kind a finnished

--- examples/postgres/index.js
+++ examples/postgres/index.js
@@ -3,8 +3,10 @@
         const methods = await require('../../lib/postgres')(
             require('./config')
         );
-        if (methods && methods['exsschema/fnLastTx'])
-            await methods['exsschema/fnLastTx']({pDevice: '34987a479f90'});
+        if (methods && methods['exsschema/fnLastTx']){
+            const res = await methods['exsschema/fnLastTx']({pDevice: '34987a479f90'});
+            console.table(res.rows);
+        }
     } catch (e) {
         console.error(e);
     }
