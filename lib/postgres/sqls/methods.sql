3f4ecc3569b3c81ff8d0ddef5faaa5063bbba85c
Elin Angelow <elin.angelov@shelly.com>
2024-02-03 20:53:00 +0200

    feat: push def val for opt arg with def value null

--- lib/postgres/sqls/methods.sql
+++ lib/postgres/sqls/methods.sql
@@ -1,18 +1,19 @@
 SELECT
     nspname "schema",
-	proname "name",
-	pronargs ninargs,
+    proname "name",
+    pronargs ninargs,
     pronargdefaults nargdefaults,
     prorettype rettype,
     proargtypes inargtypes,
     proallargtypes allargtypes,
     proargnames argnames,
-    proargmodes argmodes
+    proargmodes argmodes,
+    p.proargnames[pronargs-pronargdefaults+1:pronargs] optargnames,
+    pg_get_expr(p.proargdefaults, 0) optargdefaults
 FROM 
     pg_catalog.pg_namespace n
 JOIN 
     pg_catalog.pg_proc p ON 
     p.pronamespace = n.oid
 WHERE 
-	nspname  not IN ('pg_catalog', 'information_schema')
-
+    nspname  not IN ('pg_catalog', 'information_schema')
